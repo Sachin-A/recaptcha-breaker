5023ffb74738a53883ef2c09d1b78832455a03e7
Sachin <sachinashok.g@gmail.com>
2017-11-05 04:21:38 +0530

    Crop individual tiles according to unit size
    Individual tiles cropped/saved for individual annotation
    Complete image is cropped/saved for localisation

--- recaptchaBot.py
+++ recaptchaBot.py
@@ -1,8 +1,9 @@
 import os
+import math
 import random
+from PIL import Image
 from time import sleep, time
 from selenium import webdriver
-from PIL import Image
 import selenium.common.exceptions as e
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
@@ -15,7 +16,7 @@ def wait_between(a,b):
 	rand = random.uniform(a, b)
 	sleep(rand)
 
-def download_image(driver, element, path):
+def download_image(driver, element, path, numRows):
 	loc = element.location
 	size = element.size
 
@@ -23,13 +24,42 @@ def download_image(driver, element, path):
 
 	image = Image.open(path)
 
+	w = size['width']
+	h = size['height']
 	left = int(loc['x'])
 	top = int(loc['y'])
-	right = int(loc['x'] + size['width'])
-	bottom = int(loc['y'] + size['height'])
+	right = int(loc['x'] + w)
+	bottom = int(loc['y'] + h)
 	image = image.crop((left, top, right, bottom))
-
 	image.save(path, 'png')
+
+	captchaImage = Image.open(path)
+
+	tWidth = w / numRows
+	tHeight = h / numRows
+
+	print "Width: ",  w
+	print "Height: ",  h
+	print "Tile width: ",  tWidth
+	print "Tile height: ",  tHeight
+
+	for i in range(numRows):
+		for j in range(numRows):
+
+			print "x-min: ", int(i * tWidth)
+			print "y-min: ", int(j * tHeight)
+			print "x-max: ", int(w - ((numRows - i - 1) * tWidth))
+			print "y-max: ", int(h - ((numRows - j - 1) * tHeight))
+			tile = image.crop(
+				(
+					int(i * tWidth), \
+					int(j * tHeight), \
+					int(w - ((numRows - i - 1) * tWidth)), \
+					int(h - ((numRows - j - 1) * tHeight))
+				)
+			)
+			tilepath = os.path.splitext(path)[0] + "_" + str(i * numRows + j) + ".png"
+			tile.save(tilepath, 'png')
 
 # Cycle through all the expected conditions
 class AnyEc:
@@ -97,6 +127,8 @@ while(1):
 
 	# Gather all the img web elements in the challenge iframe only
 	candidateImages = driver.find_elements_by_tag_name("img")
+	numRows = int(math.sqrt(len(candidateImages)))
+	print "NumRows: ", numRows
 	randomIds = random.sample(range(len(candidateImages)), 3)
 
 	for i in range(len(randomIds)):
@@ -110,8 +142,11 @@ while(1):
 		theChosenOne = candidateImages[x]
 
 		if(i == 0):
-			download_image(driver, candidateImages[0], "images/" + str(challengeNumber) + ".png")
-			wait_between(2, 3)
+			download_image(driver, candidateImages[0], "images/" + str(challengeNumber) + ".png", numRows)
+			wait_between(9, 10)
+			##
+			## Todo: Tag analysis goes here
+			##
 
 		# actionChains.context_click(driver.find_elements_by_class_name("rc-image-tile-wrapper")[0]) \
 		# .send_keys(Keys.ARROW_DOWN) \
